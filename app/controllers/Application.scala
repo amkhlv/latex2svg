699e28e6dff3aeadf45ccced839eada21300ab5c
Andrei Mikhailov <amkhlv@gmail.com>
2016-07-08 01:04:21 -0300

    token to prevent CSRF attack

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,40 +2,59 @@ package controllers
 
 import play.api.libs.iteratee.Enumerator
 import play.api.mvc._
+ import play.api.Play.current
+
+case class ConfigurationException(message: String) extends Exception(message)
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  val tokenFromConf : String = current.configuration.getString("token") match {
+    case Some(x) => x
+    case None => throw new ConfigurationException("missing configuration parameter: token")
   }
 
-  def process(latex: String, size: Int, bg: String, fg: String) = Action {
+  def index = Action {
+    Ok(views.html.index("LaTeX to SVG server"))
+  }
+  def process(token: String, latex: String, size: Int, bg: String, fg: String) = Action {
     request => {
-      val svg = Convert.toSVG(latex, size, bg, fg, true)
-      if (svg.badLatex) Result(
-        header = ResponseHeader(200,
-          Map(
-            CONTENT_TYPE -> "text/plain",
-            "BystroTeX-error" -> "latex"
-          )),
-        body = Enumerator(svg.errorMessage.getBytes())
-      ) else if (svg.generalError) Result(
-        header = ResponseHeader(200,
-          Map(
-            CONTENT_TYPE -> "text/plain",
-            "BystroTeX-error" -> "general"
-          )),
-        body = Enumerator(svg.errorMessage.getBytes())
-      ) else
-      Result(
-        header = ResponseHeader(200,
-          Map(
-            CONTENT_TYPE -> "text/plain",
-            "BystroTeX-depth" -> svg.depth.toString,
-            "BystroTeX-height" -> svg.height.toString
-          )),
-        body = Enumerator(svg.svg.getBytes())
-      )
+      if (token ==  tokenFromConf) {
+        val svg = Convert.toSVG(latex, size, bg, fg, true)
+        if (svg.badLatex) Result(
+          header = ResponseHeader(200,
+            Map(
+              CONTENT_TYPE -> "text/plain",
+              "BystroTeX-error" -> "latex"
+            )),
+          body = Enumerator(svg.errorMessage.getBytes())
+        ) else if (svg.generalError) Result(
+          header = ResponseHeader(200,
+            Map(
+              CONTENT_TYPE -> "text/plain",
+              "BystroTeX-error" -> "general"
+            )),
+          body = Enumerator(svg.errorMessage.getBytes())
+        ) else
+          Result(
+            header = ResponseHeader(200,
+              Map(
+                CONTENT_TYPE -> "text/plain",
+                "BystroTeX-depth" -> svg.depth.toString,
+                "BystroTeX-height" -> svg.height.toString
+              )),
+            body = Enumerator(svg.svg.getBytes())
+          )
+      } else {
+        Result(
+          header = ResponseHeader(
+            200,
+            Map(
+              CONTENT_TYPE -> "text/plain",
+              "BystroTeX-error" -> "CSRF"
+            )),
+          body = Enumerator("CSRF detected".getBytes)
+        )
+      }
     }
   }
 
